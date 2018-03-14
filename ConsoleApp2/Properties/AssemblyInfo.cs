9a870237d279441ad64137b2c2e6f0a227d3d0fa
FaridAzayev <farid.azayev@gmail.com>
2018-03-14 12:06:47 +0400

    Update AssemblyInfo.cs

--- ConsoleApp2/Properties/AssemblyInfo.cs
+++ ConsoleApp2/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ConsoleApp2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("test comp")]
 [assembly: AssemblyProduct("ConsoleApp2")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
